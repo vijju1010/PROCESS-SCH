70cf5e1134995e6272b15f64ab0ee056f73057ad
vijju1010 <53327788+vijju1010@users.noreply.github.com>
2019-11-03 17:47:08 +0530

    Update srtf.cpp

--- srtf.cpp
+++ srtf.cpp
@@ -6,7 +6,6 @@ void swap(int *p,int *q){
 	int t=*p;
 	*p=*q;*q=t;
 }
-
 int main() 
 { int n;
 	cout<<"enter the no of process: ";
@@ -18,42 +17,29 @@ int main()
 		cout<<"enter arrival time : "<<i+1<<"th process : ";cin>>at[i];cout<<"\n";
 		cout<<"enter burst time: "<<i+1<<"th process : ";cin>>bt[i];cout<<"\n";
 	}																					//sort by ar --> prid
-		for(int i=0;i<n;i++){
-		for(int j=i+1;j<n;j++){
-			if(at[i]>at[j]){
-				swap(&at[i],&at[j]);
-				swap(&bt[j],&bt[i]);
-				swap(&pr[i],&pr[j]);
-			}
-			else if(at[i]==at[j]){
-				if(pr[i]>pr[j]){
-				swap(&at[i],&at[j]);
-				swap(&bt[j],&bt[i]);
-				swap(&pr[i],&pr[j]);}
-			}
-		}
-	}																					//print details
-		cout << "processes " << " burst Time " << " arrival time "<< endl;
+			cout << "processes " << " burst Time " << " arrival time "<< endl;
 	for (int i = 0 ; i < n ; i++) 
 	{ 
 		cout << " " << pr[i] << "\t\t" << bt[i] << "\t\t"	<< at[i] << "\t\t"<< endl; 
 	} 
+	for(int i=0;i<n;i++){		tb[i]=bt[i];
+	}
 	int co=0,cct=0,min;
- for(int i=0;i<n;i++){tb[i]=bt[i];}
-	bt[n]=INT_MAX;				//in this scheduling we need to select min burst time process so to compare burst times we take a max value and to be stored in the burst time aray at the place n so that we just use the values for comarison
+	bt[n]=INT_MAX;
 while(co!=n){
-	min=n;				
-	for(int i=0;i<n;i++){								//every time we need to select min burst time process to schedule to the cpu 
-		if(at[i]<=cct && bt[i]>0 && bt[i]<bt[min]){		//so here we select the min burst time process which are in the ready que
-			min=i;						//i.e the arrival times of process are less than the current time cct
+	min=n;
+	for(int i=0;i<n;i++){
+		if(at[i]<=cct && bt[i]>0 && bt[i]<bt[min]){
+			min=i;
 		}
 	}
-	bt[min]--;cct++;						//for now we execute on 1 unit,bcoz we cant predicted when to preempt so evry second we check the processes 
-	if(bt[min]==0){						
+	bt[min]--;cct++;
+	if(bt[min]==0){
 		co++;
-		ct[min]=cct;
+		ct[min]=cct;	
 		tat[min]=ct[min]-at[min];
 		wt[min]=tat[min]-tb[min];
+		
 	}
 }
 cout << "processes " << " burst Time " << " arrival time "<< " waiting time " << "   	 	  tat          "  << " completion time\n"; 
